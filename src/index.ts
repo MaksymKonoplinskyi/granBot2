55ba52f864edb3c5e978b39fac302925a29aad31
MaksymKonoplinskyi <gran7777777@gmail.com>
2025-06-06 14:19:02 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 const { Telegraf } = require('telegraf');
+import { Context } from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Добро пожаловать!'));
-bot.help((ctx) => ctx.reply('Отправь мне любое сообщение'));
-bot.on('text', (ctx) => ctx.reply('Вы написали: ' + ctx.message.text));
+bot.start((ctx: Context) => ctx.reply('Добро пожаловать!'));
+bot.help((ctx: Context) => ctx.reply('Отправь мне любое сообщение'));
+bot.on('text', (ctx: Context) => {
+  if (ctx.message && 'text' in ctx.message) {
+    ctx.reply('Вы написали: ' + (ctx.message as { text: string }).text);
+  }
+});
 
 bot.launch();
